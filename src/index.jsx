f8ccadd673fac126134907a42a52666ca738dcad
Kame <ev60537@meiji.ac.jp>
2018-08-26 01:46:28 +0900

    基本的な枠組み作り

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
+class CodeStage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {message: 'something'}
-    }
-
-    onChange(e) {
-        this.setState( {message: e.target.value} )
     }
 
     render() {
         return (
             <div>
-                <input type="text" onChange={this.onChange.bind(this)} />
-                <p>{this.state.message}</p>
+
+            </div>
+        )
+    }
+}
+
+class LoadCode extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="file" />
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <CodeStage />
+                <LoadCode />
             </div>
         )
     }
